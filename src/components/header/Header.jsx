20a7bfe4abd1b524dba7c4ad6a92363e42a08bd8
IskenderDev <iskenderkadyrjanov@gmail.com>
2024-09-19 18:31:05 +0600

    mainInfo v2

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,21 +1,23 @@
-import logo from "/src/assets/JashStory.svg";
 const Header = () => {
-  return (
-    <div className="flex justify-between my-[40px] px-[40px] gap-[172px]">
-      <div className="text-[24px] font-bold">
-      Jash Story
-      </div>
-      <nav>
-        <ul className="flex gap-12">
-          <li >Мировая история </li>
-          <li>История Кыргызстана</li>
-          <li>Олимпиадная история</li>
-          <li>О нас</li>
-        </ul>
-      </nav>
-      <button className="text-white bg-black rounded-[45px] text-2xl px-[27px] py-[4px]" >Войти</button>
-    </div>
-  );
-};
-
-export default Header;
+    return (
+      <header className="flex justify-between my-[40px] px-[40px] gap-[172px]">
+        <div className="text-[24px] font-bold">
+          Jash Story
+        </div>
+        <nav aria-label="Main Navigation">
+          <ul className="flex gap-12">
+            <li className="hover:text-[20px] transition-all duration-700 ease-in-out">Мировая история</li>
+            <li className="hover:text-[20px] transition-all duration-700 ease-in-out">История Кыргызстана</li>
+            <li className="hover:text-[20px] transition-all duration-700 ease-in-out">Олимпиадная история</li>
+            <li className="hover:text-[20px] transition-all duration-700 ease-in-out">О нас</li>
+          </ul>
+        </nav>
+        <button className="text-white bg-black rounded-[45px] text-2xl px-[27px] py-[4px] hover:bg-gray-800 transition-all duration-300 ease-in-out">
+          Войти
+        </button>
+      </header>
+    );
+  };
+  
+  export default Header;
+  
--- src/pages/home/ui/mainInfo/MainInfo.jsx
+++ src/pages/home/ui/mainInfo/MainInfo.jsx
@@ -2,13 +2,13 @@ import style from "./MainInfo.module.css";
 const MainInfo = () => {
   return (
     <div className={style.mainDiv}>
-      <div className="my-auto">
-        <p className="text-[52px] text-white font-medium ">
+      <div className="my-auto flex flex-col gap-[40px] justify-center items-center">
+        <p className="text-[52px] text-white font-medium text-center w-[70%]">
           Народ не знающий свою историю лишает себя корней
         </p>
         <div className="text-center flex gap-[20px] justify-center">
-          <button>Смотреть каталог</button>
-          <button>Узнать больше</button>
+          <button className="w-[234px] bg-[#CC67F8] font-medium text-xl text-white py-[5px] px-[10px] rounded-[24px]">Смотреть каталог</button>
+          <button className="w-[234px] font-medium text-xl text-white px-[15px] py-[5px] border-white border-2 rounded-[24px]">Узнать больше</button>
         </div>
       </div>
     </div>
