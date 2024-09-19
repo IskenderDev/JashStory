2557fd844daa6619db51f3902176f7de19a2016f
IskenderDev <iskenderkadyrjanov@gmail.com>
2024-09-19 16:40:47 +0600

    v1

---
+++ src/pages/home/ui/mainInfo/MainInfo.jsx
@@ -0,0 +1,13 @@
+const MainInfo = () => {
+  return (
+    <div>
+      <p>Народ не знающий свою историю лишает себя корней</p>
+      <div>
+        <button>Смотреть каталог</button>
+        <button>Узнать больше</button>
+      </div>
+    </div>
+  );
+};
+
+export default MainInfo;
